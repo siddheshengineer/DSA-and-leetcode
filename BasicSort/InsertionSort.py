2b51155ce5c0a16616ebca5c44590476f291bddc
siddhesh-its <siddheshnikam8@gmail.com>
2025-01-05 17:50:10 -0500

    insertion sort

---
+++ BasicSort/InsertionSort.py
@@ -0,0 +1,14 @@
+def insertion_sort(list):
+    for i in range (1, len(list)):
+        temp = list[i]
+        j = i - 1
+
+        while temp < list[j] and j > -1:
+            list[j+1] = list[j]
+            list[j] = temp
+            j -= 1
+
+    return list 
+
+print(insertion_sort([4,2,6,1,3,5]))
+print(insertion_sort([40,22,6,15,3,54]))
