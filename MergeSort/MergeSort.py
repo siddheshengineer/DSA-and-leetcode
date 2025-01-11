fbf6a560eae743d10ae66cac8ddae33c190f41bc
siddhesh-its <siddheshnikam8@gmail.com>
2025-01-10 20:21:07 -0500

    updated big O info

--- MergeSort/MergeSort.py
+++ MergeSort/MergeSort.py
@@ -1,5 +1,6 @@
 from Merge import merge
 
+#doesn't sort the list in place, return separate sorted list
 def merge_sort(unsorted_list):
 
     if len(unsorted_list) == 1: #stop condition for recursion, because single element list is by definition sorted
@@ -13,3 +14,8 @@ def merge_sort(unsorted_list):
     return merge(left, right)
 
 print(merge_sort([3,1,4,2]))
+
+
+##Big O
+# Space complexity is O(n), basic sorting are O(1)
+# Time complexity is O(n log n), basic sorting are O(n^2). Much better than basic sorting
