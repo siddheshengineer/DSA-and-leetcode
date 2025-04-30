f429c4bc224e146ae7a94b3e6e8dcf2fa6d5851c
N_Sidd <113104423+siddheshengineer@users.noreply.github.com>
2025-04-30 19:10:20 -0400

    1295. Find Numbers with Even Number of Digits

---
+++ String/findnum-even-digits.py
@@ -0,0 +1,36 @@
+class Solution:
+    def findNumbers(self, nums: List[int]) -> int:
+        count = 0
+
+        for num in nums:
+            num = str(num)
+            if len(num) % 2 == 0:
+                count += 1
+        return count
+
+# 1295. Find Numbers with Even Number of Digits
+# Solved
+# Easy
+# Topics
+# Companies
+# Hint
+# Given an array nums of integers, return how many of them contain an even number of digits.
+
+ 
+
+# Example 1:
+
+# Input: nums = [12,345,2,6,7896]
+# Output: 2
+# Explanation: 
+# 12 contains 2 digits (even number of digits). 
+# 345 contains 3 digits (odd number of digits). 
+# 2 contains 1 digit (odd number of digits). 
+# 6 contains 1 digit (odd number of digits). 
+# 7896 contains 4 digits (even number of digits). 
+# Therefore only 12 and 7896 contain an even number of digits.
+# Example 2:
+
+# Input: nums = [555,901,482,1771]
+# Output: 1 
+# Explanation: 
