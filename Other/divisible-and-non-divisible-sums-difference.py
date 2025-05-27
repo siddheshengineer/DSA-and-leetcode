32ced626061fafe632c6f0488cc7151f09b27e96
N_Sidd <113104423+siddheshengineer@users.noreply.github.com>
2025-05-27 18:11:54 -0400

    2894. Divisible and Non-divisible Sums Difference V2

--- Other/divisible-and-non-divisible-sums-difference.py
+++ Other/divisible-and-non-divisible-sums-difference.py
@@ -1,15 +1,27 @@
 class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
-        num1 = 0
-        num2 = 0
 
-        for i in range(1, n + 1):
-            if i % m == 0:
-                num2 += i
-            else:
-                num1 += i
+        ## Approach 2
+        k = n // m
+        # numbers divisible by m in range [1,n] form an AP: m, 2m, 3m, ..., km where k = n/m
+        # sum of first k terms of AP = k*(2*m+(k-1)m)/2 = k(k+1)*m/2
+
+        num2 = k * (k + 1)* m // 2
+        num_diff = (n * (n + 1) // 2) - (2 * num2)
+
+        return num_diff
+
+        ## Approach 1
+        # num1 = 0
+        # num2 = 0
+
+        # for i in range(1, n + 1):
+        #     if i % m == 0:
+        #         num2 += i
+        #     else:
+        #         num1 += i
         
-        return num1 - num2
+        # return num1 - num2
 
 # 2894. Divisible and Non-divisible Sums Difference
 # Solved
